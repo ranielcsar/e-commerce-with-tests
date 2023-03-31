e7021c06fa21f62d6186235d188371ff1050625e
ranoob <whyfck@gmail.com>
2023-03-31 12:14:11 -0300

    feat: product card and total value card

--- src/features/cart/components/ProductCard.tsx
+++ src/features/cart/components/ProductCard.tsx
@@ -1,4 +1,14 @@
+import { Image, Button } from '@/components/Elements'
 import { T_Product } from '@/types'
+import { convertToMonetaryValue } from '@/utils'
+
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography
+} from '@material-tailwind/react'
 
 export type ProductCardProps = {
   product: T_Product
@@ -7,6 +17,56 @@ export type ProductCardProps = {
   onRemoveFromCart: () => void
 }
 
-export function ProductCard({ product }: ProductCardProps) {
-  return <div className="border-blue-700">{product.title}</div>
+export function ProductCard({
+  product,
+  onDecrementQuantity,
+  onIncrementQuantity,
+  onRemoveFromCart
+}: ProductCardProps) {
+  return (
+    <Card className="w-full h-full max-h-[25rem]">
+      <CardHeader color="gray" className="relative h-full">
+        <Image
+          src={product.images[0]}
+          alt={`Image of: ${product.title} product`}
+        />
+
+        <button
+          className="absolute text-xl bg-transparent text-blue-gray-900 px-2 py-[5px] rounded-[50%] right-2 top-2 hover:text-red-700 hover:transition-[color] hover:duration-[0.2s]"
+          onClick={onRemoveFromCart}
+          data-testid="remove-btn"
+        >
+          <span>✖</span>
+        </button>
+      </CardHeader>
+
+      <CardBody className="text-center h-max">
+        <section className="flex justify-between items-center w-full mt-2 mb-3">
+          <Typography variant="h5" className="mb-2 w-full m-0">
+            {product.title}
+          </Typography>
+        </section>
+
+        <Typography>{product.description}</Typography>
+      </CardBody>
+
+      <Typography variant="paragraph" className="font-semibold text-center">
+        {convertToMonetaryValue(product.price * product.quantity)}
+      </Typography>
+
+      <CardFooter className="flex justify-between items-center w-64 m-auto">
+        <Button onClick={onDecrementQuantity} data-testid="decrement-btn">
+          <span className="font-bold">-</span>
+        </Button>
+
+        <p className="text-[1.4rem] text-center px-[30px] py-[5px] rounded-[5px] w-[7rem]">
+          {product.quantity}
+        </p>
+
+        <Button onClick={onIncrementQuantity} data-testid="increment-btn">
+          <span className="font-bold">+</span>
+        </Button>
+      </CardFooter>
+    </Card>
+  )
 }
--- src/features/cart/components/TotalValueCard.tsx
+++ src/features/cart/components/TotalValueCard.tsx
@@ -1,3 +1,25 @@
+import { useShop } from '@/hooks'
+import { convertToMonetaryValue } from '@/utils'
+import { Card } from '@material-tailwind/react'
+
 export function TotalValueCard() {
-  return <></>
+  const { cart } = useShop()
+
+  const totalFromEachProductInCart = cart.map(
+    (product) => product.price * product.quantity
+  )
+
+  const totalValue = totalFromEachProductInCart.reduce(
+    (previousValue, actualValue) => previousValue + actualValue,
+    0
+  )
+
+  return (
+    <Card className="absolute right-[5rem] top-[20vw] w-max flex items-center justify-between text-2xl border bg-[white] m-auto px-[35px] py-5 border-solid">
+      <span>Total:</span>
+      <strong className="text-[color:var(--accent)] ml-2.5">
+        {convertToMonetaryValue(totalValue)}
+      </strong>
+    </Card>
+  )
 }
